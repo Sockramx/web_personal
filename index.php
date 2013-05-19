35c3d19edbc3a2a80d3fe7cdad0af6681dfc7ad2
Marcos Mauricio <sockramx@gmail.com>
2013-05-19 17:26:24 -0500

    modificando ancho de la cabecera

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang='es'>
 <head>
+	<link rel="stylesheet" type="text/css" href="style.cs	s">
 	<title>SKTecnolgies</title>
 </head>
 <body>
 	<header>
-		<ul>
-			<li>OPCIONES</li>
-			<li>OPCIONES</li>
-		</ul>
+		<nav>
+			<ul>
+				<li>OPCIONES</li>
+				<li>OPCIONES</li>
+				<li>OPCIONES</li>
+				<li>OPCIONES</li>
+				<li>OPCIONES</li>
+				<li>OPCIONES</li>
+			</ul>
+		</nav>
 	</header>
 </body>
 </html>
