33160cdd0d2cf8e0b9041d7e5e3abdf467be2dd3
Marcos Mauricio <sockramx@gmail.com>
2013-05-19 14:02:59 -0500

    recreando cabecera

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang='es'>
+<head>
+	<title>SKTecnolgies</title>
+</head>
+<body>
+	<header>
+		<ul>
+			<li>OPCIONES</li>
+			<li>OPCIONES</li>
+		</ul>
+	</header>
+</body>
+</html>
